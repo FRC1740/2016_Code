c1c9c5d2f454db6e8669ae2983e3617d134c25aa
FRC Team 1740 Deus Ex Coder <cestabrooks@ledyard.net>
2016-03-31 19:59:16 -0400

    Final test code before Hartford w/ Fwd/Reverse Low bar NO ARM.

--- src/Commands/Autonomous/LowBar.cpp
+++ src/Commands/Autonomous/LowBar.cpp
@@ -1,10 +1,21 @@
-#include "LowBar.h"
+#include "../../Commands/Autonomous/LowBar.h"
+#include "../../Commands/Autonomous/Forward.h"
+#include "../../Commands/Autonomous/Reverse.h"
+#include "../../Commands/Autonomous/Pause.h"
 #include "../Drivetrain/Move.h"
 #include "../Arm/Down.h"
 #include "RoughTerrain.h"
 
 LowBar::LowBar()
 {
-	AddSequential(new Down());
-	AddSequential(new RoughTerrain(10000));
+	// AddSequential(new Down()); // No more arm on the bot
+	AddSequential(new Forward(3000));
+	AddSequential(new Pause(200));
+	AddSequential(new Reverse(3000));
+	AddSequential(new Pause(200));
+	AddSequential(new Forward(3000));
+	AddSequential(new Pause(200));
+	AddSequential(new Reverse(3000));
+	AddSequential(new Pause(200));
+	// AddSequential(new RoughTerrain(10000));
 }
